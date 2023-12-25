4b425c290e7c2898a28bc488b81a36f01e7fc0d6
Benjamin Flanders <flanderb@gmail.com>
2023-12-24 19:10:16 -0500

    Day 1 part 1 done

--- 01/advent1.dart
+++ 01/advent1.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 int combineFirstAndLastNumbers(String line){
-    String firstNum;
-    String lastNum;
-    String lineNum;
+  String firstNum;
+  String lastNum;
+  String lineNum;
 
-    firstNum = line[line.indexOf(RegExp(r'\d'))];
-    lastNum = line[line.lastIndexOf(RegExp(r'\d'))];
-    lineNum = firstNum + lastNum;
-    return int.parse(lineNum);
+  firstNum = line[line.indexOf(RegExp(r'\d'))];
+  lastNum = line[line.lastIndexOf(RegExp(r'\d'))];
+  lineNum = firstNum + lastNum;
+  return int.parse(lineNum);
 }
 
 void main() async {
@@ -19,7 +19,6 @@ void main() async {
   var lines = await myFile.readAsLines();
 
   lines.forEach((line) {
-    // print(combineFirstAndLastNumbers(line));
     total += combineFirstAndLastNumbers(line);
   });
   print(total);
