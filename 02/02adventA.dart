27892da7632ea2ae9b193eff29f07006222c3def
Benjamin Flanders <flanderb@gmail.com>
2024-01-01 22:35:32 -0500

    Finished Day 2A in dart

--- 02/02adventA.dart
+++ 02/02adventA.dart
@@ -1,25 +1,63 @@
 import 'dart:io';
 
-int combineFirstAndLastNumbers(String line){
-  String firstNum;
-  String lastNum;
-  String lineNum;
 
-  firstNum = line[line.indexOf(RegExp(r'\d'))];
-  lastNum = line[line.lastIndexOf(RegExp(r'\d'))];
-  lineNum = firstNum + lastNum;
-  return int.parse(lineNum);
+int returnGameNum(String line){
+    int gameNum = 0;
+
+    RegExp exp = RegExp(r'Game (\d+)');
+    RegExpMatch? match = exp.firstMatch(line);
+    gameNum = int.parse(match![1] ?? '');
+    return gameNum;
+}
+
+int returnLargestOfColor(String color, String line){
+
+    RegExp exp = RegExp("(\\d+) $color");
+    Iterable<Match> matches = exp.allMatches(line);
+    int largestNum = 0;
+
+    for (final Match m in matches) {
+        int currentNum = int.parse(m[1] ?? '');
+        if (currentNum > largestNum){
+            largestNum = currentNum;
+        }
+    }
+    return largestNum;
+}
+
+int returnGameNumIfPossible(String line){
+    int maxRed = 12;
+    int maxGreen = 13;
+    int maxBlue = 14;
+
+    int gameNum = returnGameNum(line);
+
+    int red = returnLargestOfColor("red", line);
+    if (red > maxRed) {
+        return 0;
+    }
+    int green = returnLargestOfColor("green", line);
+    if (green > maxGreen) {
+        return 0;
+    }
+    int blue = returnLargestOfColor("blue", line);
+    if (blue > maxBlue) {
+        return 0;
+    }
+
+    return gameNum;
 }
 
 void main() async {
-  var fileName = '02test.txt';
+  var fileName = '02input.txt';
   int total = 0;
 
   var myFile = new File(fileName);
   var lines = await myFile.readAsLines();
 
   lines.forEach((line) {
-    total += combineFirstAndLastNumbers(line);
+    int lineNum = returnGameNum(line);
+    total += returnGameNumIfPossible(line);
   });
   print(total);
 }
--- 02/play.dart
+++ 02/play.dart
@@ -1,16 +1,58 @@
 import 'dart:io';
-String line = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red';
-String? returnGameNum(String line){
-    String? gameNum = "0";
+String line = 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red';
+
+int returnGameNum(String line){
+    int gameNum = 0;
 
     RegExp exp = RegExp(r'Game (\d+)');
     RegExpMatch? match = exp.firstMatch(line);
-    gameNum = match![1];
+    gameNum = int.parse(match![1] ?? '');
     return gameNum;
 }
 
+int returnLargestOfColor(String color, String line){
+    int largest = 0;
+    RegExp exp = RegExp("(\\d+) $color");
+    Iterable<Match> matches = exp.allMatches(line);
+    int largestNum = 0;
+    // print(matches);
+    // largest = int.parse(match![1] ?? '');
+    for (final Match m in matches) {
+        int currentNum = int.parse(m[1] ?? '');
+        if (currentNum > largestNum){
+            largestNum = currentNum;
+        }
+    }
+    return largestNum;
+}
+
+int returnGameNumIfPossible(String line){
+    int maxRed = 12;
+    int maxGreen = 13;
+    int maxBlue = 14;
+
+    int gameNum = returnGameNum(line);
+
+    int red = returnLargestOfColor("red", line);
+    if (red > maxRed) {
+        return 0;
+    }
+    int green = returnLargestOfColor("green", line);
+    if (green > maxGreen) {
+        return 0;
+    }
+    int blue = returnLargestOfColor("blue", line);
+    if (blue > maxBlue) {
+        return 0;
+    }
+
+    return gameNum;
+}
+
+
+
 void main() async {
 
-    print (returnGameNum(line));
+    print (returnLargestOfColor("blue",line));
 
 }
