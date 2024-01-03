1f379c649d2b0b50db30f19c383c81fa00ce612b
Benjamin Flanders <flanderb@gmail.com>
2024-01-02 19:12:26 -0500

    Dart 02b finished

--- 02/02adventB.dart
+++ 02/02adventB.dart
@@ -1,23 +1,52 @@
 import 'dart:io';
 
+
 int returnGameNum(String line){
-    String? gameNum = "0";
+    int gameNum = 0;
 
     RegExp exp = RegExp(r'Game (\d+)');
     RegExpMatch? match = exp.firstMatch(line);
-    gameNum = int.tryParse(match![1] ?? '')
+    gameNum = int.parse(match![1] ?? '');
     return gameNum;
 }
 
+int returnLargestOfColor(String color, String line){
+
+    RegExp exp = RegExp("(\\d+) $color");
+    Iterable<Match> matches = exp.allMatches(line);
+    int largestNum = 0;
+
+    for (final Match m in matches) {
+        int currentNum = int.parse(m[1] ?? '');
+        if (currentNum > largestNum){
+            largestNum = currentNum;
+        }
+    }
+    return largestNum;
+}
+
+int returnGamePower(String line){
+
+    int gameNum = returnGameNum(line);
+
+    int red = returnLargestOfColor("red", line);
+
+    int green = returnLargestOfColor("green", line);
+
+    int blue = returnLargestOfColor("blue", line);
+
+    return red * green * blue;
+}
+
 void main() async {
-  var fileName = '02test.txt';
+  var fileName = '02input.txt';
   int total = 0;
 
   var myFile = new File(fileName);
   var lines = await myFile.readAsLines();
 
   lines.forEach((line) {
-    total += combineFirstAndLastNumbers(line);
+    total += returnGamePower(line);
   });
   print(total);
 }
